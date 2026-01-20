d0720707e36a1480a670062566c9788e4cb89fb8
Phil Darnowsky <pdarnows@broadinstitute.org>
2026-01-20 15:25:27 -0500

    list field to keep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// Fields to keep:
+// Variant:
+//  ID
+//  Filters (exome/genome)
+//  AC (exome/genome)
+//  AN (exome/genome)
+//
+//  divide by ancestry group:
+//  VEP annotation
+//  LoF curation
+//  Germline classification
+//  Flags
+//  Filters
+//  fafmax95
+//
+// Gene:
+//  Ensembl ID
+//  Region
+//  gnomAD variants
+
 fn main() {
     println!("Hello, world!");
 }
